d1f021086d234974947b8735fa2f0fe370f898e0
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-25 17:03:32 -0500

    comment

--- 3_Express-201-Middleware-Rendering/rendering.js
+++ 3_Express-201-Middleware-Rendering/rendering.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 //? 2. Set a view engine (must choose one)
 //? 3. Set a views directory, can add more than one in an array (/views by default, set implicitly)
 //? 3. Inside a route, add a 'res.render()'
-//? 4. Pass to that route the view template file you want to use, and the data you want to send to that file
+//? 4. Pass to that route the view template file you want to use, AND the data you want to send to that file
 //? 5. Express parses the view file
-//? 6. It creates the HTML page
+//? 6. It creates an HTML page from the view file and the data
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views')) // setting it explicitly
