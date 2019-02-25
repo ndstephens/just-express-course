9bf4f54efcc20f64eeb299ef05bce51aae25089e
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-25 15:15:58 -0500

    rendering engine intro

---
+++ 3_Express-201-Middleware-Rendering/rendering.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+const express = require('express')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(helmet())
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//* To use an Express Rendering Engine:
+//? 1. Do your regular Express work
+//? 2. Set a view engine (must choose one)
+//? 3. Set a views directory, can add more than one in an array (/views by default, set implicitly)
+//? 3. Inside a route, add a 'res.render()'
+//? 4. Pass to that route the view template file you want to use, and the data you want to send to that file
+//? 5. Express parses the view file
+//? 6. It creates the HTML page
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views')) // setting it explicitly
+
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
+
+//
+app.listen(3000, () => {
+  console.log('server running...')
+})
