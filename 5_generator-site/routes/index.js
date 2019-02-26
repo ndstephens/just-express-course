ddfef7621b0962cee4eaa42cb69df56a1b70deec
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-26 16:32:05 -0500

    adjusting headers

--- 5_generator-site/routes/index.js
+++ 5_generator-site/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  const date = new Date(1969, 6, 20)
+  //* use 'res.set' to SET HEADERS, 'res.get' to GET HEADERS
+  // res.set('Cache-Control', 'no-store')
+  // res.set('Content-Type', 'text/html')
+  res.set('Date', date)
 
-module.exports = router;
+  res.render('index', { title: 'Express' })
+})
+
+module.exports = router
