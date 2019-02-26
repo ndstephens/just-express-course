c684203cb90930d5e60697644df8d0cfbfe0597f
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-26 10:46:20 -0500

    Express-Generator site

---
+++ 5_generator-site/app.js
@@ -0,0 +1,47 @@
+var path = require('path')
+
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var createError = require('http-errors')
+var logger = require('morgan')
+
+// import routers
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+
+// init app
+var app = express()
+
+// view engine setup
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'views'))
+
+// middleware
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// init routes
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+// export app to be used in www.js
+module.exports = app
