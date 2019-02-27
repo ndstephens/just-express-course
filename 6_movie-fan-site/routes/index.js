83b73467691fc98dee8ee7e88766d6a4928b6640
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-26 19:33:04 -0500

    display list of movies, click on poster to redirect to movie page

--- 6_movie-fan-site/routes/index.js
+++ 6_movie-fan-site/routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+require('dotenv').config()
+const axios = require('axios')
+const express = require('express')
+
+const router = express.Router()
+
+const apiKey = process.env.MOVIEDB_API_KEY
+const apiBaseUrl = 'http://api.themoviedb.org/3'
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300'
+
+router.use((req, res, next) => {
+  //* 'imageBaseUrl' is now available in all response objects on the 'locals' object AND therefore also within all templating engines as a property
+  res.locals.imageBaseUrl = imageBaseUrl
+  next()
+  //? DON'T FORGET 'next()'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  axios
+    .get(nowPlayingUrl)
+    .then(response => {
+      // res.json(response.data)
+      res.render('index', {
+        parsedData: response.data.results,
+      })
+    })
+    .catch(err => console.log('Error', err))
+})
 
-module.exports = router;
+router.get('/movie/:id', (req, res, next) => {
+  axios
+    .get(`${apiBaseUrl}/movie/${req.params.id}?api_key=${apiKey}`)
+    .then(response => {
+      res.redirect(response.data.homepage)
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router
