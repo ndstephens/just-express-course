478d95fb40c178bc01dfe4002adaa450e526a857
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-27 19:18:55 -0500

    search API endpoints

--- 7_movie-API/routes/search.js
+++ 7_movie-API/routes/search.js
@@ -1,10 +1,39 @@
 var express = require('express')
 var router = express.Router()
 
+const movies = require('../data/movies')
+const people = require('../data/people')
+
+//? custom middleware to check that 'query' is included in URL
+const queryRequired = (req, res, next) => {
+  if (!req.query.query) {
+    res.json({ msg: 'Query is required' })
+  } else {
+    next()
+  }
+}
+
+//? apply middleware to all routes within this router
+router.use(queryRequired)
+
 //* SEARCH API -- '/search'
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/movie', function(req, res, next) {
+  const searchTerm = req.query.query
+  const results = movies.filter(
+    movie =>
+      movie.overview.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+  res.json({ results })
+})
+
+router.get('/person', function(req, res, next) {
+  const searchTerm = req.query.query
+  const results = people.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+  res.json({ results })
 })
 
 module.exports = router
